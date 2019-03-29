c76dcd1d383574ce5cbc8318599ef7231847e20b
Dmitry <kashlevdmitry@gmail.com>
2019-03-29 12:47:07 +0300

    update DataContainer

--- PryanikyTest/Model/DataContainer.swift
+++ PryanikyTest/Model/DataContainer.swift
@@ -12,11 +12,15 @@ struct DataContainer: Decodable {
     
     let text: String?
     let url: String?
+    let coverURL: String?
+    let mediaURL: String?
     let selectedID: Int?
     let variants: [SelectorVariantsData]?
     
     enum CodingKeys: String, CodingKey {
         case text, url
+        case coverURL = "coverUrl"
+        case mediaURL = "mediaUrl"
         case selectedID = "selectedId"
         case variants
     }
